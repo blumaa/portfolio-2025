d149d1116587de770c8455dfc45857ff03a345da
Aaron Blum <blumaa@gmail.com>
2023-10-03 17:02:10 +0200

    removed switch animation

--- app/animation-gallery/page.tsx
+++ app/animation-gallery/page.tsx
@@ -1,7 +1,6 @@
 import AnimationCard from "./AnimationCard";
 import OctoDude from "./animations/OctoDude";
 import AnimatedLamp from "./animations/AnimatedLamp";
-import AnimatedSwitch from "./animations/AnimatedSwitch";
 
 export default function AnimationGallery() {
   return (
@@ -9,9 +8,6 @@ export default function AnimationGallery() {
       className={`flex flex-wrap flex-col md:flex-row md:justify-center 
       md:space-x-4 items-center space-y-4 md:space-y-0 pt-4`}
     >
-      <AnimationCard bgColor="white">
-        <AnimatedSwitch />
-      </AnimationCard>
       <AnimationCard>
         <AnimatedLamp />
       </AnimationCard>

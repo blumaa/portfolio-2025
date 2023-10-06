5e2844a5e030cd2d89c6a672604927ffe9393796
Aaron Blum <blumaa@gmail.com>
2023-10-06 21:15:24 +0200

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/global.css";
 import type { Metadata } from "next";
 import { ThemeContextProvider } from "./context/theme";
@@ -16,7 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ThemeContextProvider>
-        <ThemeWrapper>{children}</ThemeWrapper>
+        <ThemeWrapper>
+          {children}
+          <Analytics />
+        </ThemeWrapper>
       </ThemeContextProvider>
     </html>
   );

0311509e06fdb288cb07abe186e91a830622056b
Aaron Blum <blumaa@gmail.com>
2023-11-26 17:24:57 +0100

    adjust christmas tree size

--- app/animation-gallery/animations/AnimatedChristmasTree/AnimatedChristmasTree.tsx
+++ app/animation-gallery/animations/AnimatedChristmasTree/AnimatedChristmasTree.tsx
@@ -74,6 +74,7 @@ const AnimatedChristmasTree = ({ play }: { play: boolean }) => {
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
         viewBox="0 0 36 36"
+        style={{ width: "100%" }}
       >
         <path
           fill="#662113"
--- app/animation-gallery/animations/AnimatedChristmasTree/AnimatedChristmasTreeWrapper.tsx
+++ app/animation-gallery/animations/AnimatedChristmasTree/AnimatedChristmasTreeWrapper.tsx
@@ -6,12 +6,14 @@ const AnimatedChristmasTreeWrapper = () => {
   const [play, setPlay] = useState(false);
 
   return (
-    <div className="flex">
-      <div className="w-1/4 flex items-center justify-center">
-        <AnimatedChristmasTreeSwitch play={play} setPlay={setPlay} />
-      </div>
-      <div className="w-3/4 ">
-        <AnimatedChristmasTree play={play} />
+    <div className="flex items-center justify-center border border-violet-300 h-full">
+      <div className="flex items-center justify-center border border-yellow-400 h-full w-full">
+        <div className="w-1/4 flex items-center justify-center">
+          <AnimatedChristmasTreeSwitch play={play} setPlay={setPlay} />
+        </div>
+        <div className="w-1/2">
+          <AnimatedChristmasTree play={play} />
+        </div>
       </div>
     </div>
   );

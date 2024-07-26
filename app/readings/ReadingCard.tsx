f1be8d03a30bc2a23d3e5ddf4473dd68c56693c9
Aaron Blum <blumaa@gmail.com>
2024-07-26 12:09:59 +0200

    Adjust readings layout (#18)
    * Update readings layout
    
    * Adjust layout

--- app/readings/ReadingCard.tsx
+++ app/readings/ReadingCard.tsx
@@ -12,18 +12,20 @@ const ReadingCard = ({ reading }: { reading: Reading }) => {
       className="md:w-1/2 border border-gray-500 p-2 rounded-sm shadow-md hover:scale-105 ease-out duration-300"
     >
       <div>
-        <div className="grid grid-cols-4 items-center justify-items-center gap-2">
-          <div className="border border-yellow-400">
-            {imageUrl && <Image src={imageUrl} alt={title} width={100} height={100} />}
+        <div className="grid grid-cols-4 items-center center gap-6">
+          <div className="col-span-1 grid grid-cols-2">
+            <div className="flex items-center justify-self-center">
+              {imageUrl && <Image src={imageUrl} alt={title} width={100} height={100} />}
+            </div>
+            <div className="p-2">
+              <div
+                className="font-bold text-2xl decoration-sky-500 underline underline-offset-1 
+                  hover:no-underline"
+              >{title}</div>
+              {author && <span>{author}</span>}
+            </div>
           </div>
-          <div>
-            <div
-              className="font-bold text-2xl decoration-sky-500 underline underline-offset-1 
-                hover:no-underline"
-            >{title}</div>
-            {author && <span>{author}</span>}
-          </div>
-          <div className="col-span-2">{description}</div>
+          <div className="col-span-3">{description}</div>
         </div>
       </div>
     </Link>

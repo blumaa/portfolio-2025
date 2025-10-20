0ab5e21e3ae3a08a7a1b97919496a41665e781be
aaron blum <blumaa@gmail.com>
2025-10-20 15:45:19 +0200

    fix rustling grass

--- app/wind/page.tsx
+++ app/wind/page.tsx
@@ -6,7 +6,7 @@ import RustlingGrass from "../animation-gallery/animations/RustlingGrass";
 const Wind = () => {
   return (
     <Box display="flex" alignItems="center" justifyContent="center" flex={1}>
-      <Box width="100%" overflow="visible" p={10}>
+      <Box width="50%" overflow="visible" p={10}>
         <RustlingGrass />
       </Box>
     </Box>

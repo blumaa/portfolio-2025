7cef11c273cd3ce89125559a0ae005e3808f4f91
aaron blum <blumaa@gmail.com>
2025-10-28 17:06:18 +0100

    wild shit

--- app/modcast/playlist.ts
+++ app/modcast/playlist.ts
@@ -9,22 +9,22 @@ export interface ModcastSegment {
 export const modcastPlaylist: ModcastSegment[] = [
   {
     type: 'intro',
-    songTitle: 'Pollos v2',
-    artistName: 'The Mond Band',
-    introScript: "Hey there, listeners! You've tuned into Modcast - the quirkiest radio show in the digital dimension! Coming up next, we've got an absolute banger for you. It's called Pollos v2 by Matt Edwards. Trust me folks, this one's gonna make your ears dance! Let's go!"
-  },
-  {
-    type: 'song',
-    audioFile: '/Pollos-v2.mp3'
-  },
-  {
-    type: 'intro',
     songTitle: '2 Hoots',
     artistName: 'The Mond Band',
-    introScript: "Wow! That was Pollos v2, wasn't it amazing? Alright, alright, don't touch that dial because we're keeping this party rolling! Next up, we've got another incredible track - it's 2 Hoots, also by Matt Edwards. This one's got a vibe that'll make you want to groove. Here we go!"
+    introScript: "Hey there, listeners! You've tuned into Modcast - the quirkiest radio show in the digital dimension! Coming up next, we've got an absolute banger for you. It's called 2 Hoots by Matt Edwards. Trust me folks, this one's gonna make your ears dance! Let's go!"
   },
   {
     type: 'song',
     audioFile: '/2hoots.mp3'
+  },
+  {
+    type: 'intro',
+    songTitle: 'Pollos v2',
+    artistName: 'The Mond Band',
+    introScript: "Wow! That was 2 Hoots, wasn't it amazing? Alright, alright, don't touch that dial because we're keeping this party rolling! Next up, we've got another incredible track - it's Pollos v2, also by Matt Edwards. This one's got a vibe that'll make you want to groove. Here we go!"
+  },
+  {
+    type: 'song',
+    audioFile: '/Pollos-v2.mp3'
   }
 ];
